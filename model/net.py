a90a090253728cd60cefa0314ee59403f92d7343
sucpark <sucpark0317@gmail.com>
2021-09-12 13:49:43 +0900

    [fix] Add bidirectional lstm & batch_first

--- model/net.py
+++ model/net.py
@@ -2,33 +2,37 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
+
 class LSTMClassifier(nn.Module):
-    def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_dim, bidirectional=False, weights=None):
+    def __init__(self, batch_size, output_size, hidden_size, vocab_size, n_layers, embedding_dim, bidirectional=False,
+                 weights=None):
         super(LSTMClassifier, self).__init__()
-        
+
         self.batch_size = batch_size
         self.output_size = output_size
         self.hidden_size = hidden_size
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
-        
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.bidirectional = bidirectional
+        self.n_layers = n_layers
+
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
         # self.embeddings.weight = nn.Parameter(weights, requires_grad=True, )
-        self.lstm = nn.LSTM(embedding_dim, hidden_size)
-        self.fc = nn.Linear(hidden_size, output_size)
-        
-    def forward(self, input_sentence, batch_size=None):
+
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, n_layers, batch_first=True, bidirectional=self.bidirectional)
+        self.fc = nn.Linear(self.hidden_size, output_size)
+
+    def forward(self, input_sentence):
         inputs = self.embeddings(input_sentence)
-        inputs = inputs.permute(1, 0, 2)
-        
-        if batch_size is None:
-            h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size)).cuda()
-            c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size)).cuda()
+
+        if self.bidirectional:
+            h_0 = Variable(torch.zeros(2 * self.n_layers, self.batch_size, self.hidden_size)).cuda()
+            c_0 = Variable(torch.zeros(2 * self.n_layers, self.batch_size, self.hidden_size)).cuda()
         else:
-            h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size)).cuda()
-            c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size)).cuda()
-            
+            h_0 = Variable(torch.zeros(self.n_layers, self.batch_size, self.hidden_size)).cuda()
+            c_0 = Variable(torch.zeros(self.n_layers, self.batch_size, self.hidden_size)).cuda()
+
         outputs, (final_hidden_state, final_cell_state) = self.lstm(inputs, (h_0, c_0))
         final_outputs = self.fc(final_hidden_state[-1])
-        
+
         return final_outputs
