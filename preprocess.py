cec5167cc10a2aa8284e1fdfaf0c9755d020fb0c
sucpark <sucpark0317@gmail.com>
2021-09-15 23:18:20 +0900

    [update] update .py files

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,27 @@
-import sys
+import argparse
 import json
 import pandas as pd
 from pathlib import Path
 from model.utils import sent_tokenize, stemming, preprocess_text
 
-sys.path.insert(0, '..')
+parser = argparse.ArgumentParser(description="Evaluating nlp classification model")
+parser.add_argument('--data_dir', default='dataset', help="Directory containing data")
+parser.add_argument('--data_name', default='SDAC')
 
 if __name__ == "__main__":
-    home_dir = Path('..')
-    data_dir = Path('dataset')
-    data_name = Path('SDAC')
+    args = parser.parse_args()
+    data_dir = Path(args.data_dir)
+    data_name = Path(args.data_name)
     train_data_name = 'sw_train.txt'
     valid_data_name = 'sw_val.txt'
     test_data_name = 'sw_test.txt'
 
-    train_data = pd.read_csv(home_dir / data_dir / data_name / train_data_name, header=None, sep='|',
+    train_data = pd.read_csv(data_dir / data_name / train_data_name, header=None, sep='|',
                              names=['speaker', 'utterance', 'tag'])
-    valid_data = pd.read_csv(home_dir / data_dir / data_name / valid_data_name, header=None, sep='|',
+    valid_data = pd.read_csv(data_dir / data_name / valid_data_name, header=None, sep='|',
                              names=['speaker', 'utterance', 'tag'])
-    test_data = pd.read_csv(home_dir / data_dir / data_name / test_data_name, header=None, sep='|',
-                            names=['speaker', 'utterance', 'tag'])
+    test_data = pd.read_csv(data_dir / data_name / test_data_name, header=None, sep='|',
+                            names=['speaker', 'utterance'])
 
     data = pd.concat([train_data['utterance'], valid_data['utterance'], test_data['utterance']], axis=0,
                      ignore_index=True)
@@ -27,58 +29,29 @@ if __name__ == "__main__":
     preprocess_pipeline = [sent_tokenize, stemming]
     data = data.apply(preprocess_text, processing_function_list=preprocess_pipeline)
 
-    # punctuation 만 있는 sentence 존재
-
-    # zero_sent_idx = [15546, 17228, 17851, 19150, 21495, 26890, 27815,
-    #                  50593, 120070, 122050, 122228, 134316, 134342]
-    #
-    # data[zero_sent_idx]
-
     word2idx = {'<PAD>': 0}
     for sent in data:
         for word in sent:
             if word not in word2idx:
                 word2idx[word] = len(word2idx)
 
-    tags = pd.concat([train_data['tag'], valid_data['tag'], test_data['tag']], axis=0, ignore_index=True)
-
+    tags = pd.concat([train_data['tag'], valid_data['tag']], axis=0, ignore_index=True)
     labels = list(tags.unique())
     label2idx = {l: i for i, l in enumerate(labels)}
 
     config = {
         "n_words": len(word2idx),
-        "n_tags": len(label2idx),
+        "n_labels": len(label2idx),
         "n_train": len(train_data),
         "n_valid": len(valid_data),
         "n_test": len(test_data)
     }
 
-    with open(home_dir / data_dir / data_name / "word2idx.json", "w") as f:
+    with open(data_dir / data_name / "word2idx.json", "w") as f:
         json.dump(word2idx, f)
 
-    with open(home_dir / data_dir / data_name / "label2idx.json", "w") as f:
+    with open(data_dir / data_name / "label2idx.json", "w") as f:
         json.dump(label2idx, f)
 
-    with open(home_dir / data_dir / data_name / "config.json", "w") as f:
+    with open(data_dir / data_name / "config.json", "w") as f:
         json.dump(config, f)
-
-    # embedding_dim = 100
-    # pretrained_embeddings = f"glove.6b.{embedding_dim}d.txt"
-    # embeddings = {}
-    # with open(data_dir / pretrained_embeddings, encoding="utf8") as file:
-    #         for line in file:
-    #             values = line.rstrip().rsplit(' ')
-    #             word = values[0]
-    #             vector = np.asarray(values[1:], dtype='float32')
-    #             embeddings[word] = vector
-
-    # embedding_matrix = np.zeros((len(word2idx), embedding_dim))
-    # for word, idx in word2idx.items():
-    #     if word in embeddings.keys():
-    #         word_embedding = embeddings[word]
-    #         embedding_matrix[idx] = word_embedding
-
-    # with open(data_dir / "embeddings.pkl", "wb") as f:
-    #     pickle.dump(embedding_matrix, f)
-
-    # # https://team-platform.tistory.com/38
--- train.py
+++ train.py
@@ -1,48 +1,42 @@
 import os
-import sys
 import argparse
 import pandas as pd
-import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
 from utils import *
 from model.metric import evaluate, acc, LSR
 from model.data import DAdataset
 from model.net import LSTMClassifier
 from model.utils import sent_tokenize, stemming, preprocess_text
-from torch.utils.data import DataLoader
+import torch.utils.data
 from torch.utils.tensorboard import SummaryWriter
 
-sys.path.insert(0, '..')
+parser = argparse.ArgumentParser(description="Training nlp classification model")
+parser.add_argument('--data_dir', default='dataset', help="Directory containing data")
+parser.add_argument('--save_dir', default='experiment')
+parser.add_argument('--data_name', default='SDAC')
+
+parser_for_training = parser.add_argument_group(title='Training')
+parser_for_training.add_argument('--epoch', default=100, type=int)
+parser_for_training.add_argument('--batch_size', dest='n_batch', default=64, type=int)
+parser_for_training.add_argument('--max_len', default=256, type=int)
+parser_for_training.add_argument('--learning_rate', dest='lr', default=1e-5, type=float)
+parser_for_training.add_argument('--embedding_dim', default=128, type=int)
+parser_for_training.add_argument('--hidden_size', default=256, type=int)
+parser_for_training.add_argument('--layer_size', dest='n_layers', default=1, type=int)
+parser_for_training.add_argument('--dropout', default=0.5, type=float)
+parser_for_training.add_argument('--summary_step', default=10000, type=int)
 
 if __name__ == "__main__":
-    home_dir = Path('..')
-    data_dir = home_dir / 'dataset' / 'SDAC'
-    save_dir = home_dir / 'experiment' / 'SDAC'
+    args = parser.parse_args()
+    data_dir = Path(args.data_dir) / args.data_name
+    save_dir = Path(args.save_dir) / args.data_name
     train_data_name = 'sw_train.txt'
     valid_data_name = 'sw_val.txt'
-    test_data_name = 'sw_test.txt'
-    # pretrained_embeddings_name = 'embeddings.pkl'
     token2idx_name = 'word2idx.json'
     label2idx_name = 'label2idx.json'
-    config_name = 'config.json'
 
     if not os.path.exists(save_dir):
         os.makedirs(save_dir, exist_ok=True)
-
-    args = {
-        "epochs": 100,
-        "n_batch": 64,
-        "max_len": 256,
-        "lr": 1e-5,
-        "summary_step": 10000,
-        "embedding_dim": 128,
-        "hidden_size": 256,
-        "n_layers": 1,
-    }
-    args = argparse.Namespace(**args)
-
-    # with open(data_dir / pretrained_embeddings_name, 'rb') as f:
-    #     pretrained_embeddings = pickle.load(f)
 
     with open(data_dir / token2idx_name, 'r') as f:
         token2idx = json.load(f)
@@ -50,13 +44,8 @@ if __name__ == "__main__":
     with open(data_dir / label2idx_name, 'r') as f:
         label2idx = json.load(f)
 
-    with open(data_dir / config_name, 'r') as f:
-        config = json.load(f)
-
     idx2token = {i: t for t, i in token2idx.items()}
     idx2label = {i: l for l, i in label2idx.items()}
-
-    # 데이터 전처리
 
     train_data = pd.read_csv(data_dir / train_data_name, header=None, sep='|', names=['speaker', 'utterance', 'tag'])
     valid_data = pd.read_csv(data_dir / valid_data_name, header=None, sep='|', names=['speaker', 'utterance', 'tag'])
@@ -64,73 +53,48 @@ if __name__ == "__main__":
     x_train, y_train = train_data['utterance'], train_data['tag']
     x_valid, y_valid = valid_data['utterance'], valid_data['tag']
 
-    text_preprocess_pipeline = [sent_tokenize, stemming]  # 이전에 해줬던 전처리 과정과 동일하게 해줘야 함
-
+    text_preprocess_pipeline = [sent_tokenize, stemming]
     x_train = x_train.apply(preprocess_text, processing_function_list=text_preprocess_pipeline)
     x_valid = x_valid.apply(preprocess_text, processing_function_list=text_preprocess_pipeline)
 
-    # 학습 데이터 구성
-
     x_train = list(convert_token_to_idx(x_train, token2idx))
     x_valid = list(convert_token_to_idx(x_valid, token2idx))
-
     y_train = list(convert_label_to_idx(y_train, label2idx))
     y_valid = list(convert_label_to_idx(y_valid, label2idx))
 
     tr_ds = DAdataset(x_train, y_train)
-    tr_dl = DataLoader(tr_ds, batch_size=args.n_batch, collate_fn=tr_ds.collate_fn, shuffle=True, drop_last=True)
-
+    tr_dl = torch.utils.data.DataLoader(tr_ds, batch_size=args.n_batch, collate_fn=tr_ds.collate_fn, drop_last=True)
     val_ds = DAdataset(x_valid, y_valid)
-    val_dl = DataLoader(val_ds, batch_size=args.n_batch, collate_fn=val_ds.collate_fn, shuffle=True, drop_last=True)
+    val_dl = torch.utils.data.DataLoader(val_ds, batch_size=args.n_batch, collate_fn=val_ds.collate_fn, drop_last=True)
 
-    # 모델 생성
-
-    learning_rate = args.lr
-    batch_size = args.n_batch
     vocab_size = len(token2idx)
-    embedding_dim = args.embedding_dim
-    hidden_size = args.hidden_size
     output_size = len(label2idx)
-    n_layers = args.n_layers
-    dropout = 0.7
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = LSTMClassifier(batch_size, output_size, hidden_size, vocab_size, n_layers, embedding_dim, device,
-                           dropout=dropout, bidirectional=True)  # , weights=torch.from_numpy(pretrained_embeddings))
-
-    loss_fn = LSR(epsilon=0.1, num_classes=output_size)
-
     if device.type == 'cuda':
         torch.cuda.empty_cache()
 
+    model = LSTMClassifier(args.n_batch, output_size, args.hidden_size, vocab_size, args.n_layers,
+                           args.embedding_dim, device, dropout=args.dropout, bidirectional=True)
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)
 
+    loss_fn = LSR(epsilon=0.1, num_classes=output_size)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)
     writer = SummaryWriter(f'{save_dir}/runs')
     checkpoint_manager = CheckpointManager(save_dir)
     summary_manager = SummaryManager(save_dir)
 
-    # 모델 학습
-
     best_val_loss = 1e+10
-    train_acc = []
-    valid_acc = []
-    train_losses = []
-    valid_losses = []
-    x_error_check = []  # Issue 에 대한 디버깅 코드
+    train_acc, valid_acc = [], []
+    train_losses, valid_losses = [], []
 
-    for epoch in tqdm(range(args.epochs), desc='epochs'):
+    for epoch in tqdm(range(args.epoch), desc='epochs'):
         tr_loss = 0
         tr_acc = 0
         model.train()
 
         for step, mb in tqdm(enumerate(tr_dl), desc='steps', total=len(tr_dl)):
             x_mb, y_mb = map(lambda elm: elm.to(device), mb)
-
-            x_error_check.append(x_mb)
-            if len(x_error_check) > 2:
-                del x_error_check[0]
-
             optimizer.zero_grad()
             y_hat_mb = model(x_mb)
             loss = loss_fn(y_hat_mb, y_mb)
@@ -140,7 +104,7 @@ if __name__ == "__main__":
             with torch.no_grad():
                 mb_acc = acc(y_hat_mb, y_mb)
 
-            clip_gradient(model, 1e-1)
+            # clip_gradient(model, 1e-1)
 
             tr_loss += loss.item()
             tr_acc += mb_acc.item()
@@ -182,56 +146,28 @@ if __name__ == "__main__":
                 checkpoint_manager.save_checkpoint(state, 'best.tar')
                 best_val_loss = val_loss
 
+    best_point = valid_losses.index(min(valid_losses)) + 1
     fig = plt.figure(figsize=(10, 8))
     plt.plot(range(1, len(train_losses) + 1), train_losses, label='Training Loss')
     plt.plot(range(1, len(valid_losses) + 1), valid_losses, label='Validation Loss')
-
-    minposs = valid_losses.index(min(valid_losses)) + 1
-    plt.axvline(minposs, linestyle='dotted', color='r', label='Best Model Checkpoint')
-
+    plt.axvline(best_point, linestyle='dotted', color='r', label='Best Model Checkpoint')
     plt.title('Loss Curve')
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
-    plt.show()
     fig.savefig(f'{save_dir}/loss_plot.png', bbox_inches='tight')
 
     fig = plt.figure(figsize=(10, 8))
     plt.plot(range(1, len(train_acc) + 1), train_acc, label='Training Accuracy')
     plt.plot(range(1, len(valid_acc) + 1), valid_acc, label='Validation Accuracy')
-
-    minposs = valid_losses.index(min(valid_losses)) + 1
-    plt.axvline(minposs, linestyle='--', color='r', label='Best Model Checkpoint')
-
+    plt.axvline(best_point, linestyle='--', color='r', label='Best Model Checkpoint')
     plt.title('Accuracy Plot')
     plt.xlabel('epochs')
     plt.ylabel('accuracy')
     plt.grid(True)
     plt.legend()
     plt.tight_layout()
-    plt.show()
     fig.savefig(f'{save_dir}/accuracy_plot.png', bbox_inches='tight')
 
-    test_data = pd.read_csv(data_dir / test_data_name, header=None, sep='|', names=['speaker', 'utterance', 'tag'])
-
-    x_test, y_test = test_data['utterance'], test_data['tag']
-
-    text_preprocess_pipeline = [sent_tokenize, stemming]
-    x_test = x_test.apply(preprocess_text, processing_function_list=text_preprocess_pipeline)
-
-    x_test = list(convert_token_to_idx(x_test, token2idx))
-    y_test = list(convert_label_to_idx(y_test, label2idx))
-
-    test_ds = DAdataset(x_test, y_test)
-    test_dl = DataLoader(test_ds, batch_size=64, collate_fn=test_ds.collate_fn, drop_last=True)
-
-    summ = evaluate(model, test_dl, {'loss': loss_fn, 'acc': acc}, device)
-
-    summary_manager = SummaryManager(save_dir)
-    summary_manager.load('summary.json')
-    summary_manager.update(summ)
-    summary_manager.save('summary.json')
-
-    print('loss: {:3f}, acc: {:.2%}'.format(summ['loss'], summ['acc']))
